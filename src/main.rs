285bc1b1dd47ce9ff7f21c1f96989b290c006179
Pedro Ivo Carneiro Macieira <1170554@isep.ipp.pt>
2020-10-16 12:34:14 +0100

    Work already complete

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use rltk::{Console, GameState, Rltk, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
+
+struct State {
+    ecs: World,
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: u16,
+    fg: RGB,
+    bg: RGB,
+}
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+fn main() {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()
+        .expect("Couldn't build rltk");
+    let mut gs = State { ecs: World::new() };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 25 })
+            .with(Renderable {
+                glyph: 1,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
+    rltk::main_loop(context, gs).expect("Main loop failed");
+}
