b89e9434b5dbe6253f9f41604f82566147519cc2
Pedro Ivo Carneiro Macieira <1170554@isep.ipp.pt>
2020-10-22 00:53:47 +0100

    Basic ECS system and player movement

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,18 @@ struct State {
     ecs: World,
 }
 
+impl State {
+    fn run_systems(&mut self) {
+        self.ecs.maintain();
+    }
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        input(self, ctx);
+        
+        self.run_systems();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
         for (pos, render) in (&positions, &renderables).join() {
@@ -30,6 +40,32 @@ struct Position {
     y: i32,
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
+fn attempt_move_player(x: i32, y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + x));
+        pos.y = min(49, max(0, pos.y + y));
+    }
+}
+
+fn input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {},
+        Some(key) => match key {
+            VirtualKeyCode::Left => attempt_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => attempt_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => attempt_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => attempt_move_player(0, 1, &mut gs.ecs),
+            _ => {},
+        }
+    }
+}
+
 fn main() {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -39,6 +75,7 @@ fn main() {
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -47,6 +84,7 @@ fn main() {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player{})
         .build();
     for i in 0..10 {
         gs.ecs
