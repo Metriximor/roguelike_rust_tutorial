2f58ea067c751ca13c795ac0dfefdce6d0d32306
Pedro Ivo Carneiro Macieira <1170554@isep.ipp.pt>
2020-11-01 02:02:01 +0000

    Walking around, random map and folder restructure

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use rltk::{Console, GameState, Rltk, VirtualKeyCode, RGB};
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
-use specs_derive::Component;
-use std::cmp::{max, min};
 
-struct State {
+mod components;
+use components::*;
+mod map;
+use map::*;
+mod player;
+use player::*;
+
+pub struct State {
     ecs: World,
 }
 
@@ -16,52 +21,14 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        input(self, ctx);
-        
+        player_input(self, ctx);
         self.run_systems();
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
-        }
-    }
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: u16,
-    fg: RGB,
-    bg: RGB,
-}
-
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component, Debug)]
-struct Player {}
-
-fn attempt_move_player(x: i32, y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + x));
-        pos.y = min(49, max(0, pos.y + y));
-    }
-}
-
-fn input(gs: &mut State, ctx: &mut Rltk) {
-    match ctx.key {
-        None => {},
-        Some(key) => match key {
-            VirtualKeyCode::Left => attempt_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => attempt_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => attempt_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => attempt_move_player(0, 1, &mut gs.ecs),
-            _ => {},
         }
     }
 }
@@ -76,6 +43,7 @@ fn main() {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+    gs.ecs.insert(new_map());
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -84,18 +52,7 @@ fn main() {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
-        .with(Player{})
+        .with(Player {})
         .build();
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 25 })
-            .with(Renderable {
-                glyph: 1,
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .build();
-    }
     rltk::main_loop(context, gs).expect("Main loop failed");
 }
---
+++ src/player.rs
@@ -0,0 +1,33 @@
+use rltk::{VirtualKeyCode, Rltk};
+use std::cmp::{max, min};
+use specs::prelude::*;
+use super::components::{Position, Player};
+use super::map::{TileType, xy_index};
+use super::{State};
+
+pub fn attempt_move_player(x: i32, y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        let destination_index = xy_index(pos.x + x, pos.y + y);
+        if map[destination_index] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + x));
+            pos.y = min(49, max(0, pos.y + y));
+        }
+    }
+}
+
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => attempt_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => attempt_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => attempt_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => attempt_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
